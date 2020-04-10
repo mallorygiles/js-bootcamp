dca97a02a1bd5cfa8373e19e9707895a20dbb8e1
Mallory Giles <malloryngiles@gmail.com>
2020-04-10 10:45:44 -0400

    add filters module

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,5 +1,20 @@
-import { getNotes, createNote } from './notes'
+import { getNotes, createNote, removeNote, updateNote } from './notes'
+import { getFilters, setFilters } from './filters'
 
-console.log(getNotes())
-createNote()
-console.log(getNotes())
+// console.log(getNotes())
+// createNote()
+//removeNote('be181a89-1c06-467c-aafb-46b3fcce83ac')
+
+// updateNote('cb87f7c2-807e-4cc5-8f65-20ebdbfa4abf', {
+//     title: 'My Note title',
+//     body: 'This is the body for my note'
+// })
+//console.log(getNotes())
+
+
+console.log(getFilters())
+setFilters({
+    searchText: 'office',
+    sortBy: 'byCreated'
+})
+console.log(getFilters())
--- notes-app/src/notes.js
+++ notes-app/src/notes.js
@@ -36,6 +36,71 @@ const saveNotes = () => {
     localStorage.setItem('notes', JSON.stringify(notes))
  }
 
+ const removeNote = (id) => {
+    const noteIndex = notes.findIndex((note) => note.id === id)
+    if(noteIndex > -1){
+      notes.splice(noteIndex, 1)
+      saveNotes()
+    }
+  }
+
+  //Sort notes by one of three types
+const sortNotes = (sortBy) => {
+    if(sortBy === 'byEdited'){
+      return notes.sort((a,b) => {
+        if(a.updatedAt > b.updatedAt){
+          return -1
+        } else if(a.updatedAt < b.updatedAt){
+          return 1
+        } else{
+          return 0
+        }
+      })
+    } else if(sortBy === 'byCreated'){
+      return notes.sort((a,b) => {
+        if(a.createdAt > b.createdAt){
+          return -1
+        } else if(a.createdAt < b.createdAt){
+          return 1
+        } else{
+          return 0
+        }
+      })
+    } else if(sortBy === 'alphabetically'){
+      return notes.sort((a, b) => {
+        if(a.title.toLowerCase() < b.title.toLowerCase()){
+          return -1
+        } else if(a.title.toLowerCase() > b.title.toLowerCase()){
+          return 1
+        } else {
+          return 0
+        }
+      })
+    } else {
+      return notes
+    }
+  }
+
+const updateNote = (id, updates) => {
+    const note = notes.find((note) => note.id === id)
+
+    if(!note) {
+        return
+    }
+
+    if(typeof updates.title === 'string') {
+        note.title = updates.title
+        note.updatedAt = moment.valueOf()
+    }
+
+    if(typeof updates.body === 'string') {
+        note.body = updates.body
+        note.updatedAt = moment.valueOf()
+    }
+    saveNotes()
+
+}
+
 notes = loadNotes()
 
-export { getNotes, createNote }
+export { getNotes, createNote, removeNote, sortNotes, updateNote }
