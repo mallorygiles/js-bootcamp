b5768e1dfad5bc2d122be9fc11a93098007a7829
Mallory Giles <malloryngiles@gmail.com>
2020-04-10 14:56:10 -0400

    implement todos.js

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -5,13 +5,23 @@
 
 // Add necessary imports
 import {getFilters, setFilters} from './filters'
+import { getTodos, createTodo, removeTodo, toggleTodo } from './todos'
 
-console.log(getFilters())
+/* console.log(getFilters())
 setFilters({
     searchText: 'item',
     hideCompleted: true
 })
-console.log(getFilters())
+console.log(getFilters()) */
+console.log(getTodos()[1].complete)
+/* createTodo('todo 1')
+createTodo('todo 2')
+createTodo('todo 3')
+createTodo('todo 4')
+createTodo('todo 5') */
+//removeTodo('19b6a136-d2ba-4783-91fa-a06987ffd51d')
+toggleTodo('87833267-2195-441f-95e1-e324ca34654e')
+console.log(getTodos()[1].complete)
 
 
 
